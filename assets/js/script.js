c3fdc767e676097bcaa58177a64b6a9c4debd106
eatmorevegetabl <moises@Moisess-MacBook-Pro.local>
2023-01-15 15:57:31 -0700

    added CSS & JS file

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+//Functions
+//async functions
+
+https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={minutely,hourly}&appid={API key}
+
+http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},US&limit=2&appid={API_key}
