f62b8720fcacb9c573c65efc244c741b4ce644e8
eatmorevegetabl <moises@Moisess-MacBook-Pro.local>
2023-01-17 22:39:02 -0700

    nearly complete project

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# weatherDashboard
+# Weather Dashboard
+
+## Description
+
+The weather dashboard displays a city's name, current date, temperature, conditions, humidity and wind speed. It also displays a 5-day forecast for that city. The user has to enter the name of a city and click "Submit". If the user enters a city name that is invalid, an error message will display.
+
+## Visuals
+
+<img class="screenshot" src="assets/images/Screenshot.png">
+
+## Link
+
+Link to the completed webpage:
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,65 @@
-//Functions
-//async functions
+var button = document.querySelector('.button');
+var inputValue = document.querySelector('.inputValue');
+var cityName = document.querySelector('.city-name');
+var date = document.querySelector('.date');
+var conditions = document.querySelector('.conditions');
+var temperature = document.querySelector('.temperature');
+var humidity = document.querySelector('.humidity');
+var windSpeed = document.querySelector('.wind-speed');
 
-https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={minutely,hourly}&appid={API key}
+var day1 = document.querySelector('.day-1');
+var day2 = document.querySelector('.day-2');
+var day3 = document.querySelector('.day-3');
+var day4 = document.querySelector('.day-4');
+var day5 = document.querySelector('.day-5');
 
-http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},US&limit=2&appid={API_key}
+//localStorage.getItem("City") = document.querySelector('.btn');
+
+button.addEventListener('click', function () {
+  fetch(
+    'https://api.openweathermap.org/data/2.5/weather?q=' +
+      inputValue.value +
+      '&APPID=e4455fb88575d6b953035ab71e6e15ab&units=imperial'
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      var cityValue = data['name'];
+      var condValue = data['weather']['0']['description'];
+      var tempValue = data['main']['temp'];
+      var humdValue = data['main']['humidity'];
+      var windValue = data['wind']['speed'];
+
+      cityName.innerHTML = cityValue;
+      temperature.innerHTML = 'Temperature: ' + tempValue + ' °F';
+      conditions.innerHTML = 'Conditions: ' + condValue;
+      humidity.innerHTML = 'Humidity: ' + humdValue + ' %';
+      windSpeed.innerHTML = 'Wind Speed: ' + windValue + ' mph';
+      date.innerHTML = moment().format('MMMM Do YYYY');
+    })
+
+    .catch((err) => alert('City Name Invalid!'));
+  localStorage.setItem('City', inputValue.value);
+  getForecastData();
+});
+
+function getForecastData() {
+  fetch(
+    'https://api.openweathermap.org/data/2.5/forecast?q=' +
+      inputValue.value +
+      '&APPID=e4455fb88575d6b953035ab71e6e15ab&units=imperial'
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      var day1Value = data['list']['7']['main']['temp'];
+      var day2Value = data['list']['15']['main']['temp'];
+      var day3Value = data['list']['23']['main']['temp'];
+      var day4Value = data['list']['31']['main']['temp'];
+      var day5Value = data['list']['39']['main']['temp'];
+
+      day1.innerHTML = 'Day 1: ' + day1Value + ' °F';
+      day2.innerHTML = 'Day 2: ' + day2Value + ' °F';
+      day3.innerHTML = 'Day 3: ' + day3Value + ' °F';
+      day4.innerHTML = 'Day 4: ' + day4Value + ' °F';
+      day5.innerHTML = 'Day 5: ' + day5Value + ' °F';
+    });
+}
